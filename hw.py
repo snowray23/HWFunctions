37ac0845cac0f4cb2ba067523884807de8160376
Raymond Zheng <raymondzheng1994@gmail.com>
2024-03-24 11:37:03 -0400

    first commit

---
+++ hw.py
@@ -0,0 +1,47 @@
+#1
+def add(x, y):
+    return x + y
+
+def subtract (x, y):
+    return x - y
+
+def multiply (x, y):
+    return x * y
+
+def divide (x, y):
+    if y == 0:
+        return "error"
+    else:
+        return x / y
+
+
+num1 = int(input("enter number "))
+num2 = int(input("enter number2 "))
+operation = input ("what is your operation? add,subtract,multiply, divide ")
+
+
+#2
+def add_item(shopping_list, item):
+    shopping_list.append(item)
+    print(f"{item} has been added to your list")
+
+def remove_item(shopping_list, item):
+    shopping_list.remove(item)
+    print(f"{item} has been removed from your list")
+
+def print_list(shopping_list):
+    print("shopping list")
+
+
+#3
+def avg_grade(grades_list):
+    return sum(grades_list) / len(grades_list)
+
+def highest_grade(grades_list):
+ return max(grades_list)
+
+def lowest_grade(grades_list):
+    return min(grades_list)
+    
+def sorted_list(grades_list):
+    return sorted(grades_list)
